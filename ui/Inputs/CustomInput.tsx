be92cce841072ebcc236ece1107c36e94bf68300
Abhisek Rakshit <abhisek.rakshit@webskitters.com>
2023-12-02 08:55:18 +0530

    init

--- ui/Inputs/CustomInput.tsx
+++ ui/Inputs/CustomInput.tsx
@@ -1,26 +1,27 @@
 import { CommonMuiInputProps } from "@/interface/common.interface";
-import { TextField } from "@mui/material";
 import FormControl from "@mui/material/FormControl";
 import FormHelperText from "@mui/material/FormHelperText";
 import OutlinedInput from "@mui/material/OutlinedInput";
-import { MutableRefObject, forwardRef, useId } from "react";
+import { forwardRef, MutableRefObject, useId } from "react";
 
-const CustomInputWith_forwardRef = ({
-  error = false,
-  label,
-  value,
-  onChange,
-  helperText = "",
-  placeholder = "",
-  type,
-  startAdornment,
-  endAdornment
-}: CommonMuiInputProps) => {
+const CustomInputWith_forwardRef = (
+  {
+    error = false,
+    label,
+    value,
+    onChange,
+    helperText = "",
+    placeholder = "",
+    type,
+    startAdornment,
+    endAdornment
+  }: CommonMuiInputProps
+) => {
   const id = useId();
   return (
     <FormControl error={error} fullWidth>
       {label}
-      <TextField
+      <OutlinedInput
         error={error}
         id={id}
         type={type}
